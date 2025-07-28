8a71bc6999f1579c7217d33d6c299c6748412bff
“Zuzana <zuzana.moskvic@gmail.com>
2025-07-28 21:44:36 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "./App.css";
+import data from "./countries.json";
 
 function App() {
+  const [countries, setCountrie] = useState(data);
   return <div className="App"></div>;
 }
+
 export default App;
